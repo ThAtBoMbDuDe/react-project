5e09f1d9f245f417834468d84a6ca32eeb47bce9
Jacob <117200950+ThAtBoMbDuDe@users.noreply.github.com>
2023-01-13 10:27:21 -0500

    Website no longer crashes

--- src/Main/CardList/Card/Card.jsx
+++ src/Main/CardList/Card/Card.jsx
@@ -34,42 +34,42 @@ console.log(searchedCards)
 
   return (
     <>
-    <div className="beerCard">
+    {searchedCards[0] && <div className="beerCard">
       <h1>{searchedCards[0].name}</h1>
       <h3>{searchedCards[0].tagline}</h3>
       <img src={searchedCards[0].image_url} alt='Beer Logo'></img>
       <h4>{searchedCards[0].description}</h4>
-    </div>
-    <div className="beerCard">
+    </div>}
+    {searchedCards[1] && <div className="beerCard">
       <h1>{searchedCards[1].name}</h1>
       <h3>{searchedCards[1].tagline}</h3>
       <img src={searchedCards[1].image_url} alt='Beer Logo'></img>
       <h4>{searchedCards[1].description}</h4>
-    </div>
-    <div className="beerCard">
+    </div>}
+    {searchedCards[2] && <div className="beerCard">
       <h1>{searchedCards[2].name}</h1>
       <h3>{searchedCards[2].tagline}</h3>
       <img src={searchedCards[2].image_url} alt='Beer Logo'></img>
       <h4>{searchedCards[2].description}</h4>
-    </div>
-    <div className="beerCard">
+    </div>}
+    {searchedCards[3] && <div className="beerCard">
       <h1>{searchedCards[3].name}</h1>
       <h3>{searchedCards[3].tagline}</h3>
       <img src={searchedCards[3].image_url} alt='Beer Logo'></img>
       <h4>{searchedCards[3].description}</h4>
-    </div>
-    <div className="beerCard">
+    </div>}
+    {searchedCards[4] && <div className="beerCard">
       <h1>{searchedCards[4].name}</h1>
       <h3>{searchedCards[4].tagline}</h3>
       <img src={searchedCards[4].image_url} alt='Beer Logo'></img>
       <h4>{searchedCards[4].description}</h4>
-    </div>
-    <div className="beerCard">
+    </div>}
+    {searchedCards[5] && <div className="beerCard">
       <h1>{searchedCards[5].name}</h1>
       <h3>{searchedCards[5].tagline}</h3>
       <img src={searchedCards[5].image_url} alt='Beer Logo'></img>
       <h4>{searchedCards[5].description}</h4>
-    </div>
+    </div>}
 
 
     </>
